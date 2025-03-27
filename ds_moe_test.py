3911d8ecdcd964896d66e327069a80183e66be02
weibaozi <guo361270936@gmail.com>
2025-03-27 01:33:10 -0700

    test

--- ds_moe_test.py
+++ ds_moe_test.py
@@ -335,6 +335,7 @@ def main():
     #     final_df = pd.DataFrame({'Source Text':sources,'Generated Text':predictions,'Actual Text':actuals})
     #     final_df.to_csv('./output/predictions.csv')
     #     print('Output Files generated for review')
+    
 trained_model, tokenizer = main()
 trained_model.save_pretrained(output_dir)
 tokenizer.save_pretrained(output_dir)
